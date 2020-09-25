3b59f277f2052045b87445ee46060fcd025d0c46
Rohan-shakya <rohanshakya254@gmail.com>
2020-09-25 12:12:04 +0545

    added Payment class

---
+++ src/modules/Payment.ts
@@ -0,0 +1,11 @@
+export class Payment {
+  constructor(
+    readonly recipient: string,
+    private details: string,
+    public amount: number
+  ) {}
+
+  format() {
+    return `${this.recipient} is owed ${this.amount} for ${this.details}`;
+  }
+}
