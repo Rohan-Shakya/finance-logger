9fa96580fb0c9567c3dfd4f7ef224d809982fe83
Rohan-shakya <rohanshakya254@gmail.com>
2020-09-25 12:10:50 +0545

    added Invoice class

--- public/app.js
+++ public/app.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('It worked!!');
+const form = document.querySelector('.new-item-form');
+// inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#toFrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value);
+    console.log(toFrom.value);
+    console.log(details.value);
+    console.log(amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('It worked!!');
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(type.value);
+  console.log(toFrom.value);
+  console.log(details.value);
+  console.log(amount.valueAsNumber);
+});
