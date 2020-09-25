5303c6caf593de7c019dd65c3d328b181e89ef4e
Rohan-shakya <rohanshakya254@gmail.com>
2020-09-25 12:22:33 +0545

    instantiate new Invoice and Payment

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+import { Invoice } from './modules/Invoice.js';
+import { Payment } from './modules/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -10,8 +14,20 @@ const ul = document.querySelector('ul')!;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(type.value);
-  console.log(toFrom.value);
-  console.log(details.value);
-  console.log(amount.valueAsNumber);
+
+  let values: [string, string, number]; //tuples
+
+  values = [toFrom.value, details.value, amount.valueAsNumber];
+
+  let doc: HasFormatter;
+
+  if (type.value === 'invoice') {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+
+  toFrom.value = '';
+  details.value = '';
+  amount.value = '';
 });
